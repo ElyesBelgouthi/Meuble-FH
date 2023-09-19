fe3141faa948346409267943622ccb339f75a073
ElyesBelgouthi <belelyesbel@gmail.com>
2023-09-19 02:23:51 +0100

    finsishing catalogue page

--- src/app/pages/catalogue/catalogue.component.ts
+++ src/app/pages/catalogue/catalogue.component.ts
@@ -8,6 +8,8 @@ import { Component } from '@angular/core';
 export class CatalogueComponent {
   p: number = 1;
 
+  filters: string[] = [];
+
   items = [
     {
       title: 'salon',
@@ -187,4 +189,38 @@ export class CatalogueComponent {
       image: '../../../assets/images/home-img-1.jpg',
     },
   ];
+
+  categories = [
+    {
+      name: 'Category',
+      extended: false,
+      subcategories: [
+        { name: 'Chaise', selected: false },
+        { name: 'Bureau', selected: false },
+        { name: 'Comptoir', selected: false },
+        { name: 'Étagère', selected: false },
+      ],
+    },
+    {
+      name: 'Type',
+      expanded: false,
+      subcategories: [
+        { name: 'Moderne', selected: false },
+        { name: 'Classique', selected: false },
+      ],
+    },
+  ];
+  filterExpanded: boolean = false;
+
+  toggleFilter() {
+    this.filterExpanded = !this.filterExpanded;
+  }
+
+  toggleAccordion(category: any) {
+    category.expanded = !category.expanded;
+  }
+
+  toggleSubcategory(subcategory: any) {
+    subcategory.selected = !subcategory.selected;
+  }
 }
