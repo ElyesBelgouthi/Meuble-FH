10bc50ac9449b488a2c6fbaf0fbf50500ebd2738
ElyesBelgouthi <belelyesbel@gmail.com>
2023-09-22 00:10:11 +0100

    finshing cart page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,7 +14,12 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes, {
+      enableTracing: false,
+      scrollPositionRestoration: 'top',
+    }),
+  ],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import ItemService from 'src/app/services/item.service';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.css']
+  styleUrls: ['./cart.component.css'],
 })
-export class CartComponent {
+export class CartComponent implements OnInit {
+  isCartEmpty: boolean = true;
+  cartItems!: any[];
+  p: number = 1;
 
+  constructor(private itemService: ItemService) {}
+
+  ngOnInit(): void {
+    this.cartItems = this.itemService.items;
+  }
 }
